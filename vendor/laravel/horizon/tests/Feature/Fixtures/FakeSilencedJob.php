c49d870fbe1c4b9c821a7ba7584dd6d17ca88664
sokcheait <sokcheaitcoputer122@gmail.com>
2024-10-06 11:31:50 +0700

    review code

--- vendor/laravel/horizon/tests/Feature/Fixtures/FakeSilencedJob.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Laravel\Horizon\Tests\Feature\Fixtures;
-
-use Laravel\Horizon\Contracts\Silenced;
-
-class FakeSilencedJob implements Silenced
-{
-}
--- vendor/laravel/horizon/tests/Feature/Fixtures/SilencedMailable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Laravel\Horizon\Tests\Feature\Fixtures;
-
-use Illuminate\Contracts\Mail\Mailable;
-
-interface SilencedMailable extends Mailable
-{
-}
